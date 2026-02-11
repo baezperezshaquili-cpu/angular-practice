4fde2d551fa760371327793f5814a58385553774
Shaquil Baez <baezperezshaquili@gmail.com>
2026-02-11 19:18:46 -0400

    More Changes

--- PatientList/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
+++ PatientList/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762711e937b7567ff920c04b0606c89b109cea2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db714c16d302382219d08470fef0f6a07121d25")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
